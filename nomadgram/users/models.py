0aa382d3ee4703dec991d01652c2b2057c03398c
wphestiraid <wphestiraid@gmail.com>
2017-12-13 22:09:38 +0900

    Creating the User Model

--- nomadgram/users/models.py
+++ nomadgram/users/models.py
@@ -8,9 +8,19 @@ from django.utils.translation import ugettext_lazy as _
 @python_2_unicode_compatible
 class User(AbstractUser):
 
+    GENDER_CHOICE = (
+        ('male', 'Male'),
+        ('femail', 'Female'),
+        ('not-specified', 'Not specified')
+    )
+
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    website = models.URLField(null=True)
+    bio = models.TextField(null=True)
+    phone = models.CharField(max_length=32, null=True)
+    gender = models.CharField(max_length=32, choices=GENDER_CHOICE, null=True)
 
     def __str__(self):
         return self.username
