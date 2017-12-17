a8b82b10cb1595377bf754a02683f578c7c3c765
wphestiraid <wphestiraid@gmail.com>
2017-12-17 21:04:44 +0900

    Registering the Models in the admim

--- nomadgram/images/admin.py
+++ nomadgram/images/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Comment)
+class CommentAdin(admin.ModelAdmin):
+    pass
