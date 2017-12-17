49141f8971d3a3e46696cbb566e87b21a2b9a8c6
wphestiraid <wphestiraid@gmail.com>
2017-12-17 21:21:23 +0900

    Customizing the Django Admin

--- nomadgram/images/admin.py
+++ nomadgram/images/admin.py
@@ -3,14 +3,49 @@ from . import models
 
 @admin.register(models.Image)
 class ImageAdmin(admin.ModelAdmin):
-    pass
+
+    list_display_links = (
+        'caption',
+    )
+
+    search_fields = (
+        'caption',
+        'location',
+    )
+
+    list_filter = (
+        'location',
+        'creator'
+    )
+
+    list_display = (
+        'file',
+        'caption',
+        'location',
+        'creator',
+        'created_at',
+        'updated_at',
+    )
 
 
 @admin.register(models.Like)
 class LikeAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = (
+        'creator',
+        'image',
+        'created_at',
+        'updated_at',
+    )
 
 
 @admin.register(models.Comment)
 class CommentAdin(admin.ModelAdmin):
-    pass
+
+    list_display = (
+        'message',
+        'creator',
+        'image',
+        'created_at',
+        'updated_at',
+    )
