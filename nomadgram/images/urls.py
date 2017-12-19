13d634f4e183caeb31a1357c1d7a6f3641c7c6b4
wphestiraid <wphestiraid@gmail.com>
2017-12-19 21:26:59 +0900

    Viewing all the Comments and Likes

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^all/$', views.ListAllImages.as_view(), name='all_images'),
+    url(r'^comments/$', views.ListAllComments.as_view(), name='all_comments'),
+    url(r'^likes/$', views.ListAllLikes.as_view(), name='all_likes'),
 ]
