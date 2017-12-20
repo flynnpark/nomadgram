46457a5d04abd6fd1bd41c366fc84fb671a478ae
wphestiraid <wphestiraid@gmail.com>
2017-12-20 21:13:07 +0900

    Getting the User Feed part Two

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -1,12 +1,6 @@
 from rest_framework import serializers
 from . import models
-
-class CommentSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = models.Comment
-        fields = '__all__'
-
+from nomadgram.users import models as user_models
 
 class LikeSerializer(serializers.ModelSerializer):
 
@@ -15,10 +9,34 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class FeedUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = user_models.User
+        fields = (
+            'username',
+            'profile_image',
+        )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    creator = FeedUserSerializer()
+
+    class Meta:
+        model = models.Comment
+        fields = (
+            'id',
+            'message',
+            'creator',
+        )
+
+
+
 class ImageSerializer(serializers.ModelSerializer):
 
     comments = CommentSerializer(many=True)
-    likes = LikeSerializer(many=True)
+    creator = FeedUserSerializer()
 
     class Meta:
         model = models.Image
@@ -28,5 +46,6 @@ class ImageSerializer(serializers.ModelSerializer):
             'location',
             'caption',
             'comments',
-            'likes',
+            'like_count',
+            'creator',
         )
