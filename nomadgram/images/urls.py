0c2ade3a3c9e1032e7ba9a800ff95bc51f6a443f
wphestiraid <wphestiraid@gmail.com>
2017-12-20 22:49:47 +0900

    Liking an Image

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Feed.as_view(), name='feed'),
-    url(r'(?P<image_id>\w+)/like/', views.LikeImage.as_view(), name='like_image'),
+    url(r'(?P<image_id>[0-9]+)/like/', views.LikeImage.as_view(), name='like_image'),
 ]
--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -28,6 +28,18 @@ class LikeImage(APIView):
 
     def get(self, request, image_id, format=None):
 
-        print(image_id)
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=404)
+
+        new_like = models.Like.objects.create(
+            creator=user,
+            image=found_image,
+        )
+
+        new_like.save()
 
         return Response(status=200)
