b08117b92959e4801a79c8afba40d88235781969
wphestiraid <wphestiraid@gmail.com>
2017-12-21 20:15:42 +0900

    Restricting Likes

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
 
 class Feed(APIView):
@@ -33,13 +34,21 @@ class LikeImage(APIView):
         try:
             found_image = models.Image.objects.get(id=image_id)
         except models.Image.DoesNotExist:
-            return Response(status=404)
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-        new_like = models.Like.objects.create(
-            creator=user,
-            image=found_image,
-        )
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image,
+            )
+            preexisting_like.delete()
 
-        new_like.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except models.Like.DoesNotExist:
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image,
+            )
+            new_like.save()
 
-        return Response(status=200)
+            return Response(status=status.HTTP_201_CREATED)
