c350673759ab15dc59867bb1453848a1974ed215
wphestiraid <wphestiraid@gmail.com>
2017-12-23 11:51:54 +0900

    Following a User

--- nomadgram/users/serializers.py
+++ nomadgram/users/serializers.py
@@ -7,6 +7,7 @@ class ExploreUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = (
+            'id',
             'profile_image',
             'username',
             'name',
--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^explore$', views.ExploreUsers.as_view(), name='list'),
+    url(r'^(?P<user_id>[0-9]+)/follow/$', views.FollowUser.as_view(), name='follow_user'),
 ]
