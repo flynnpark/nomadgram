5d7f161a5b5a20b66efae343403ebef8fcd8e4c1
wphestiraid <wphestiraid@gmail.com>
2017-12-24 15:51:24 +0900

    Unfollowing User

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^explore$', views.ExploreUsers.as_view(), name='list'),
     url(r'^(?P<user_id>[0-9]+)/follow/$', views.FollowUser.as_view(), name='follow_user'),
+    url(r'^(?P<user_id>[0-9]+)/unfollow/$', views.UnFollowUser.as_view(), name='unfollow_user'),
 ]
