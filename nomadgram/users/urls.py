f2e6b0b4f765763cee58c8811ee5548f23739b2c
wphestiraid <wphestiraid@gmail.com>
2017-12-25 17:21:21 +0900

    Getting Following List

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^(?P<user_id>[0-9]+)/unfollow/$', views.UnFollowUser.as_view(), name='unfollow_user'),
     url(r'^(?P<username>\w+)/$', views.UserProfile.as_view(), name='user_profile'),
     url(r'^(?P<username>\w+)/followers/$', views.UserFollowers.as_view(), name='user_followers'),
+    url(r'^(?P<username>\w+)/following/$', views.UserFollowing.as_view(), name='user_following'),
 ]
--- nomadgram/users/views.py
+++ nomadgram/users/views.py
@@ -79,3 +79,19 @@ class UserFollowers(APIView):
         serializer = serializers.ListUserSerializer(user_followers, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class UserFollowing(APIView):
+
+    def get(self, request, username, format=None):
+
+        try:
+            found_user = models.User.objects.get(username=username)
+        except models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        user_following = found_user.following.all()
+
+        serializer = serializers.ListUserSerializer(user_following, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
