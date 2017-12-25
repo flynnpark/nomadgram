e55bdb6ad8f50004e661e8818d7f2bd0bf8d43a7
wphestiraid <wphestiraid@gmail.com>
2017-12-25 20:10:16 +0900

    Search users by username

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     url(r'^explore$', views.ExploreUsers.as_view(), name='list'),
     url(r'^(?P<user_id>[0-9]+)/follow/$', views.FollowUser.as_view(), name='follow_user'),
     url(r'^(?P<user_id>[0-9]+)/unfollow/$', views.UnFollowUser.as_view(), name='unfollow_user'),
-    url(r'^(?P<username>\w+)/$', views.UserProfile.as_view(), name='user_profile'),
     url(r'^(?P<username>\w+)/followers/$', views.UserFollowers.as_view(), name='user_followers'),
     url(r'^(?P<username>\w+)/following/$', views.UserFollowing.as_view(), name='user_following'),
+    url(r'^search/$', views.Search.as_view(), name='search'),
+    url(r'^(?P<username>\w+)/$', views.UserProfile.as_view(), name='user_profile'),
 ]
