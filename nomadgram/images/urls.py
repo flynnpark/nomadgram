397366b9447fe0500e7fa7efdaaef11b3a853019
wphestiraid <wphestiraid@gmail.com>
2017-12-27 12:59:20 +0900

    Getting the image likes

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.Feed.as_view(), name='feed'),
     url(r'^(?P<image_id>[0-9]+)/$', views.ImageDetail.as_view(), name='image_detail'),
-    url(r'^(?P<image_id>[0-9]+)/like/$', views.LikeImage.as_view(), name='like_image'),
-    url(r'^(?P<image_id>[0-9]+)/unlike/$', views.UnLikeImage.as_view(), name='like_image'),
-    url(r'^(?P<image_id>[0-9]+)/comment/$', views.CommentOnImage.as_view(), name='comment_image'),
-    url(r'^(?P<image_id>[0-9]+)/comment/(?P<comment_id>[0-9]+)/$', views.ModerateComment.as_view(), name='comment_image'),
+    url(r'^(?P<image_id>[0-9]+)/likes/$', views.LikeImage.as_view(), name='like_image'),
+    url(r'^(?P<image_id>[0-9]+)/unlikes/$', views.UnLikeImage.as_view(), name='like_image'),
+    url(r'^(?P<image_id>[0-9]+)/comments/$', views.CommentOnImage.as_view(), name='comment_image'),
+    url(r'^(?P<image_id>[0-9]+)/comments/(?P<comment_id>[0-9]+)/$', views.ModerateComment.as_view(), name='comment_image'),
     url(r'^comments/(?P<comment_id>[0-9]+)/$', views.Comment.as_view(), name='comment'),
     url(r'^search/$', views.Search.as_view(), name='search'),
 ]
--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
+from nomadgram.users import models as user_models
+from nomadgram.users import serializers as user_serializers
 from nomadgram.notifications import views as notification_views
 
 class Feed(APIView):
@@ -32,6 +34,16 @@ class Feed(APIView):
 
 
 class LikeImage(APIView):
+
+    def get(self, request, image_id, format=None):
+
+        likes = models.Like.objects.filter(image__id=image_id)
+        like_creator_ids = likes.values('creator_id')
+        users = user_models.User.objects.filter(id__in=like_creator_ids)
+
+        serializer = user_serializers.ListUserSerializer(users, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, image_id, format=None):
 
