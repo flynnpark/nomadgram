8d27b0180250621ddfd26daa3240d49fa556702a
wphestiraid <wphestiraid@gmail.com>
2017-12-27 12:38:08 +0900

    Including Myself on the Feed

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -19,6 +19,11 @@ class Feed(APIView):
             for image in user_images:
                 image_list.append(image)
 
+        my_images = user.images.all()[:2]
+
+        for image in my_images:
+            image_list.append(image)
+
         sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
 
         serializer = serializers.ImageSerializer(sorted_list, many=True)
