9acd8e621957642aa644b3933932948908eec4d5
wphestiraid <wphestiraid@gmail.com>
2017-12-29 12:37:44 +0900

    Setting up JWT

--- config/settings/base.py
+++ config/settings/base.py
@@ -282,3 +282,14 @@ ADMIN_URL = r'^admin/'
 ALLOWED_HOSTS = ["*"]
 
 TAGGIT_CASE_INSENSITIVE = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
--- config/urls.py
+++ config/urls.py
@@ -6,13 +6,11 @@ from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
-
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
 
     # User management
+    url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^users/', include('nomadgram.users.urls', namespace='users')),
     url(r'^images/', include('nomadgram.images.urls', namespace='images')),
     url(r'^notifications/', include('nomadgram.notifications.urls', namespace='notifications')),
