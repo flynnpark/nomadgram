848487f93fbea726697567e94fde8e02bb2f3ab2
wphestiraid <wphestiraid@gmail.com>
2018-01-01 16:32:43 +0900

    Serving Webpack Bundles with Django - Port 3000 is developing server - Port 8000 is production server - With proxy to find REST service from django to development - ReactAppView is created by Webpack

--- config/settings/base.py
+++ config/settings/base.py
@@ -53,8 +53,9 @@ THIRD_PARTY_APPS = [
     'rest_framework.authtoken', # REST auth
     'taggit', # Tags for the photos
     'taggit_serializer', # Tag serializer
-    'rest_auth',
+    'rest_auth', # REST auth
     'rest_auth.registration', # Enable registration
+    'corsheaders', # To accept requests from React
 ]
 
 # Apps specific for this project go here.
@@ -74,6 +75,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -197,6 +199,7 @@ STATIC_URL = '/static/'
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(APPS_DIR.path('static')),
+    str(ROOT_DIR.path('frontend', 'build', 'static')),
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
@@ -297,3 +300,5 @@ REST_FRAMEWORK = {
 
 REST_USE_JWT = True
 ACCOUNT_LOGOUT_ON_GET = True
+
+CORS_ORIGIN_ALLOW_ALL = True
--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 from rest_framework_jwt.views import obtain_jwt_token
+from nomadgram import views
 
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
@@ -18,8 +19,10 @@ urlpatterns = [
     url(r'^images/', include('nomadgram.images.urls', namespace='images')),
     url(r'^notifications/', include('nomadgram.notifications.urls', namespace='notifications')),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^', views.ReactAppView.as_view()),
 
     # Your stuff: custom urls includes go here
+
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
