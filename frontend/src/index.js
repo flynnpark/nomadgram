cd45211353592dbe6061902687014977e452d6b9
wphestiraid <wphestiraid@gmail.com>
2018-01-04 23:32:35 +0900

    Installing Redux Dev Tools - Remove Reactotron...

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,6 @@ import store, { history } from "redux/configureStore";
 import "index.css";
 import App from "App";
 
-import "ReactotronConfig";
-
 ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
