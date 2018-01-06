4344cadc25556cec684eb3aa7a61321f53c7d5d6
wphestiraid <wphestiraid@gmail.com>
2018-01-06 21:11:58 +0900

    Handling Authentication with Redux and React Router

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,15 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import styles from "components/App/styles.scss";
 import Footer from "components/Footer";
 
-class App extends Component {
-    render() {
-        return (
-          <div className={styles.App}>
-            <Footer />
-          </div>
-        );
-    };
-};
+const App = props => [
+    // Nav
+    // Routes
+    props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+    <Footer key={3} />
+];
+
+const PrivateRoutes = props => (
+    <Switch>
+        <Route exact path="/" render={() => "Feed"} />
+        <Route exact path="/explore" render={() => "Explore"} />
+    </Switch>
+);
+
+const PublicRoutes = props => (
+    <Switch>
+        <Route exact path="/" render={() => "Login"} />
+        <Route exact path="/forgot" render={() => "Password"} />
+    </Switch>
+)
 
 export default App;
