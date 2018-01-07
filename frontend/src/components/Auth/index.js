0e141c34fa8b59c4c10784a3d9ad795dd1bfec36
wphestiraid <wphestiraid@gmail.com>
2018-01-07 15:20:19 +0900

    Auth Component part One

---
+++ frontend/src/components/Auth/index.js
@@ -0,0 +1,10 @@
+import { connect } from "react-redux";
+import Container from "./container";
+
+// Add all the actions for :
+// Log in
+// Sign up
+// Recover Password
+// Check username, password, email
+
+export default connect()(Container);
