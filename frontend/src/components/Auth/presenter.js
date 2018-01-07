11872fcea1e3d783e6a19fc2ae128977d95e9832
wphestiraid <wphestiraid@gmail.com>
2018-01-07 16:51:18 +0900

    Auth Component part Three - Add react-ionicons

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,36 +1,30 @@
 import React from "react";
 import styles from "./styles.scss";
+import { LoginForm, SignupForm } from "components/AuthForms";
 
 const Auth = (props, context) => (
     <main className={styles.auth}>
         <div className={styles.column}>
         </div>
         <div className={styles.column}>
+            <div className={`${styles.whiteBox} &{styles.formBox}`}>
+                <div className={styles.logo}></div>
+                {props.action === "login" && <LoginForm />}
+                {props.action === "signup" && <SignupForm />}
+            </div>
             <div className={styles.whiteBox}>
-                {(() => {
-                    switch (props.action) {
-                        case "login":
-                            return (
-                                <p>
-                                    Don't have an account?{" "}
-                                    <span className={styles.changeLink} onClick={props.changeAction}>
-                                        Sign up
-                                    </span>
-                                </p>
-                            );
-                        case "signup":
-                            return (
-                                <p>
-                                    Have an account?{" "}
-                                    <span className={styles.changeLink} onClick={props.changeAction}>
-                                        Log in
-                                    </span>
-                                </p>
-                            );
-                        default:
-                            return null;
-                    }
-                })()}
+                {props.action === "login" && (<p>
+                    Don't have an account?{" "}
+                    <span className={styles.changeLink} onClick={props.changeAction}>
+                        Sign up
+                    </span>
+                </p>)}
+                {props.action === "signup" && (<p>
+                    Have an account?{" "}
+                    <span className={styles.changeLink} onClick={props.changeAction}>
+                        Log in
+                    </span>
+                </p>)}
             </div>
             <div className={styles.appBox}>
                 <span>Get the app</span>
