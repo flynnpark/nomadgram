4e80b54d1ea0e115ff787f811655583877074667
wphestiraid <wphestiraid@gmail.com>
2018-01-15 12:55:54 +0900

    Logout Action - 백엔드에서 401에러 리턴하는 부분을 모두 400으로 수정 - 프론트엔드에서 토큰이 망가졌거나 유효하지 않을 경우 토큰 삭제, 로그아웃 처리

--- nomadgram/users/views.py
+++ nomadgram/users/views.py
@@ -88,7 +88,7 @@ class UserProfile(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         if found_user.username != user.username:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         else:
             serializer = serializers.UserProfileSerializer(found_user, data=request.data, partial=True)
@@ -182,7 +182,7 @@ class ChangePassword(APIView):
                 return Response(status=status.HTTP_400_BAD_REQUEST)
 
         else:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class FacebookLogin(SocialLoginView):
