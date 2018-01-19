27897344e132167a04cc7df4f23780a421419aae
wphestiraid <wphestiraid@gmail.com>
2018-01-19 20:48:43 +0900

    Controlling Comment Component - 엔터 키를 무효화 시킴 - 회원 가입 및 로그인 때 적용했던 방법 적용

--- frontend/src/components/CommentBox/container.js
+++ frontend/src/components/CommentBox/container.js
@@ -2,9 +2,33 @@ import React, { Component } from "react";
 import CommentBox from "./presenter";
 
 class Container extends Component {
+    state = {
+        comment: ""
+    };
+
     render() {
-        return <CommentBox />;
+        return (
+            <CommentBox
+                {...this.state}
+                handleInputChange={this._handleInputChange}
+                handleKeyPress={this._handleKeyPress} />
+        );
     }
+
+    _handleInputChange = (event) => {
+        const { target: { value } } = event;
+        this.setState({
+            comment: value
+        });
+    };
+
+    _handleKeyPress = event => {
+        const { key } = event;
+        if (key === "Enter") {
+            event.preventDefault();
+        }
+        console.log(key);
+    };
 }
 
 export default Container;
--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -7,12 +7,21 @@ const CommentBox = (props, context) => (
     <form className={styles.commentBox}>
         <Textarea
             className={styles.input}
-            placeholder={context.t("Add a comment...")} />
+            placeholder={context.t("Add a comment...")}
+            value={props.comment}
+            onChange={props.handleInputChange}
+            onKeyPress={props.handleKeyPress} />
     </form>
 );
 
 CommentBox.contextTypes = {
     t: PropTypes.func.isRequired
+};
+
+CommentBox.propTypes = {
+    handleInputChange: PropTypes.func.isRequired,
+    handleKeyPress: PropTypes.func.isRequired,
+    comment: PropTypes.string.isRequired,
 };
 
 export default CommentBox;
