a0c06c15c7054a1a7d0324940dcd44094ae40424
wphestiraid <wphestiraid@gmail.com>
2018-02-23 23:37:20 +0900

    Nomadgrapp: Creating the getSearch - 검색어가 없을 경우를 위해 백엔드 수정

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -166,11 +166,12 @@ class Search(APIView):
             hashtags = hashtags.split(",")
             images = models.Image.objects.filter(tags__name__in=hashtags).distinct()
             serializer = serializers.CountImageSerializer(images, many=True)
-
             return Response(data=serializer.data, status=status.HTTP_200_OK)
 
         else:
-            return Response(status=HTTP_400_BAD_REQUEST)
+            images = models.Images.objects.all()[:20]
+            serializer = serializers.CommentSerializer(images, many=True)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 class ModerateComment(APIView):
