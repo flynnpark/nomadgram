1438deb6057c5bdecaccdb6daa6084c34bc2faae
wphestiraid <wphestiraid@gmail.com>
2018-03-06 19:59:49 +0900

    Creating is_self and following SerializerMethods

--- nomadgram/users/serializers.py
+++ nomadgram/users/serializers.py
@@ -7,10 +7,12 @@ from nomadgram.images import serializers as images_serializers
 
 class UserProfileSerializer(serializers.ModelSerializer):
 
-    images = images_serializers.CountImageSerializer(many=True, read_only=True)
+    images = images_serializers.ImageSerializer(many=True, read_only=True)
     post_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
+    is_self = serializers.SerializerMethodField()
+    is_following = serializers.SerializerMethodField()
 
     class Meta:
         model = models.User
@@ -24,8 +26,26 @@ class UserProfileSerializer(serializers.ModelSerializer):
             'post_count',
             'followers_count',
             'following_count',
-            'images'
+            'images',
+            'is_self',
+            'is_following'
         )
+
+    def get_is_self(self, user):
+        if 'request' in self.context:
+            request = self.context['request']
+            if user.id == request.user.id:
+                return True
+            else:
+                 return False
+        return False
+
+    def get_is_following(self, obj):
+        if 'request' in self.context:
+            request = self.context['request']
+            if obj in request.user.following.all():
+                return True
+        return False
 
 
 class ListUserSerializer(serializers.ModelSerializer):
